6ce044bf59bb52420d3eb73d5a67c2baf1f15ccc
TacoTechnica <adrisj7@gmail.com>
2022-03-06 00:36:42 +0000

    did a silly

--- src/legacy.ts
+++ src/legacy.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 
-import { startDatabase, stopDatabase, downloadBeatmapPackage } from "./db";
+import { downloadBeatmapPackage } from "./db";
 
 // Processes each line of a "legacy download list" file, to help us deal with this.
 const downloadLegacyPackages = async (downloadsFilePath : string, processDownload : (download : string) => void) => {
