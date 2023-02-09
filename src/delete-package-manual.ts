ff8eaf33f5da825860f25edfec050f8e4a713625
TacoTechnica <adrisj7@gmail.com>
2023-02-09 18:09:09 +0000

    Add helper scripts to download and delete packages manually

---
+++ src/delete-package-manual.ts
@@ -0,0 +1,11 @@
+import { deletePackage } from './db'
+
+const args = process.argv.slice(2);
+
+if (args.length != 1) {
+    console.log("Usage: delete-package-manual <package-filename.zip>")
+} else {
+    console.log("DELETING ", args[0])
+    deletePackage(args[0])
+}
+
---
+++ src/download-package-manual.ts
@@ -0,0 +1,11 @@
+import { downloadBeatmapPackage } from './db'
+
+const args = process.argv.slice(2);
+
+if (args.length != 2) {
+    console.log("Usage: download-package-manual <url-to-package-zip> <name-of-package>")
+} else {
+    console.log("downloading url ", args[0], "to ", args[1])
+    downloadBeatmapPackage(args[0], args[1])
+}
+
